6bcc361c924c4953801d1a44066309941253d126
Gytis <Gytis.kymantas@kilo.health>
2023-04-11 15:46:57 +0300

    final push

--- src/app/tag/[...slug]/page.js
+++ src/app/tag/[...slug]/page.js
@@ -1,16 +1,80 @@
 // import { getAllSlugs } from '@/lib/posts';
-
-import Tag from '@/pages/tag';
+import { BLOG_POSTS } from '@/components/blogData';
+import Link from 'next/link';
+import { POSTS_PER_PAGE } from '@/lib/utils/constants';
 
 export default async function ArchivePages({ params }) {
   const { slug = [] } = params;
+  let currentPage = parseInt(slug[1]) === 1 ? 0 : slug[1];
+  const query = decodeURIComponent(slug[0]).replace(/%20/g, ' ');
+  const filteredArray = BLOG_POSTS.filter((obj) => obj.tags.includes(query));
+
+  let totalPages = Math.ceil(filteredArray?.length / POSTS_PER_PAGE);
+  let hasPrevPage = currentPage - 1 > 0;
+
+  let lastPage = Number(currentPage) === totalPages;
 
   return (
     <div className='prose max-w-prose'>
-      <h1 className='bg-green-50 p-32 text-center font-mono text-xl leading-loose tracking-wide text-red-700'>
-        /tag/{slug.join('/')}
-        <Tag page={slug.join('/')} />
-      </h1>
+      <div>
+        <h1 className='bg-green-50 p-32 text-center font-mono text-xl leading-loose tracking-wide text-red-700'>
+          /tag/{slug.join('/')}
+          <br />
+          <br />
+          total found posts: {filteredArray?.length} that contain "{query}"
+        </h1>
+      </div>
+      {filteredArray
+        ?.slice(
+          currentPage === 1 ? 0 : currentPage * 10,
+          currentPage * 10 + POSTS_PER_PAGE
+        )
+        .map(({ title, slug }, index) => {
+          return (
+            <li key={index}>
+              <span>{currentPage * POSTS_PER_PAGE + index + 1}.&nbsp;</span>
+              <Link href={`/${slug}`}>{title}</Link>
+            </li>
+          );
+        })}
+      <div className='space-y-2 bg-green-200 p-8'>
+        <nav className='flex justify-between'>
+          {!hasPrevPage && (
+            <button
+              className='umami--click--prev-posts cursor-auto disabled:opacity-50'
+              disabled={!hasPrevPage}
+            >
+              Previous
+            </button>
+          )}
+          {hasPrevPage && (
+            <Link
+              href={
+                currentPage - 1 === 1
+                  ? `/tag/${query}/1`
+                  : `/tag/${query}/${currentPage - 1}`
+              }
+            >
+              <button>Previous</button>
+            </Link>
+          )}
+          <span>
+            {currentPage === 0 ? 1 : currentPage} of {totalPages}
+          </span>
+          <Link href={`/tag/${query}/${parseInt(slug[1]) + 1}`}>
+            <button
+              className={
+                lastPage &&
+                'umami--click--next-posts cursor-auto disabled:opacity-50'
+              }
+              disabled={lastPage}
+            >
+              Next
+            </button>
+          </Link>
+          {/* )} */}
+        </nav>
+      </div>
     </div>
   );
 }
--- src/pages/tag.js
+++ src/pages/tag.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Pagination } from '@/components/Pagination';
 import { POSTS_PER_PAGE } from '@/lib/utils/constants';
 import Link from 'next/link';
 import { BLOG_POSTS } from '@/components/blogData';
@@ -7,27 +6,42 @@ import { BLOG_POSTS } from '@/components/blogData';
 const Tag = ({ page = 0 }) => {
   let currentPage = page;
 
+  const tagsArray = [];
+
+  for (let i = 0; i < BLOG_POSTS.length; i++) {
+    const post = BLOG_POSTS[i];
+
+    for (let j = 0; j < post.tags.length; j++) {
+      const tag = post.tags[j];
+
+      tagsArray.push(tag);
+    }
+  }
+
+  const tagCounts = tagsArray.reduce((acc, tag) => {
+    acc[tag] = acc[tag] ? acc[tag] + 1 : 1;
+    return acc;
+  }, {});
+
+  const uniqueTags = Object.entries(tagCounts).map(([tag, count]) => ({
+    tag,
+    count,
+  }));
+
   return (
     <div>
-      <h2>This is the Tag page</h2>
+      <h2>These are the unique tags available</h2>
       <ul>
-        {BLOG_POSTS?.slice(
-          currentPage === 0 ? 0 : currentPage * 10,
-          currentPage * 10 + POSTS_PER_PAGE
-        ).map((post, index) => {
+        {uniqueTags.map(({ tag, count }, index) => {
           return (
-            <li key={post.slug}>
+            <li key={index}>
               <span>{currentPage * POSTS_PER_PAGE + index + 1}.&nbsp;</span>
-              <Link href={post.slug}>{post.tags.join(', ')}</Link>
+              <Link href={`/tag/${tag}/1`}>{tag}</Link> there are {count} in
+              total
             </li>
           );
         })}
       </ul>
-      <Pagination
-        currentPage={Number(currentPage)}
-        totalPages={Math.ceil(BLOG_POSTS?.length / POSTS_PER_PAGE)}
-        tag='tag'
-      />
     </div>
   );
 };
